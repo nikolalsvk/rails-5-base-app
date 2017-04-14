8a6f8466cc0e628658604254ca13026208125b07
nikolalsvk <nikolaseap@gmail.com>
2017-04-14 00:24:29 +0000

    Test Welcome controllers index action

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,4 +4,10 @@ class WelcomeControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get index" do
+    get root_url
+
+    assert_response :success
+  end
 end
